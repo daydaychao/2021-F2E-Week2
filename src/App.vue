425e069fead8673f64f51b94cc0024d589fd9897
daydaychao <chiachiachao@gmail.com>
2021-11-17 19:48:25 +0800

    style: 💄 gps

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
 
 import Header from '@/components/TheHeader.vue'
-import GPS from '@/components/TheGPS.vue'
+import GPS from '@/components/GPS.vue'
 </script>
 
 <template>
---
+++ src/components/TheOhNo.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts"></script>
+
+<template>
+  <section>
+    <div>哎呀 ! 這裡似乎 沒有自行車道呢</div>
+    <div>試試別的地方地方吧， 或者去投訴當地政府吧 (´･ω･`)</div>
+  </section>
+</template>
+
+<style scoped></style>
