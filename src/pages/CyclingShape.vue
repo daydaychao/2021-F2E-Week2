e643a9b3d8dde5f427d94cd62a9ada3bb743bfc1
daydaychao <chiachiachao@gmail.com>
2021-11-21 22:19:31 +0800

    refactor: 💡 自行車道選擇區域

--- src/pages/CyclingShape.vue
+++ src/pages/CyclingShape.vue
@@ -1,77 +1,103 @@
 <script setup lang="ts">
 import '@/assets/styles/root.css'
-import { ref, reactive, watch } from 'vue'
+import { ref, unref, reactive, watch, computed, SelectHTMLAttributes } from 'vue'
 import { useBikeStore } from '@/stores/bike'
 import { getEnumValues, changeName } from '@/tools/'
 import { StationAndAvailabilityCityNameZhTW, StationAndAvailabilityCityName, CyclingShapeCityName, CyclingShapeCityNameZhTW } from '@/types'
 import Map from '@/components/mapCyclingShape.vue'
-
-
-defineProps<{ msg: string }>()
+import { filter } from 'ramda'
+import { TypeCyclingShape } from '@/types'
 
 const bikeStore = useBikeStore()
-const cityList = getEnumValues(StationAndAvailabilityCityName)
-const cityListZhTW = getEnumValues(StationAndAvailabilityCityNameZhTW)
-const selectCity = ref<keyof typeof StationAndAvailabilityCityName>() //select
 
-let geometry = [];
+const cityList = getEnumValues(CyclingShapeCityName)
+const cityListZhTW = getEnumValues(CyclingShapeCityNameZhTW)
+const selectCity = ref<keyof typeof CyclingShapeCityNameZhTW>() //select
+const selectTown = ref('') //select
+
+const townList = computed(() => {
+  return bikeStore.getCyclingShapeTown
+})
+
+let CyclingShapeFilteredList = bikeStore.getCyclingShape
+
+let geometry = [] //FIXME who are you?
 
 // api抓取data
 function getCyclingShapeData() {
   if (selectCity.value) bikeStore.getCyclingShapeData(selectCity.value)
 }
 
+// 選都市
 async function handleSelect() {
   await getCyclingShapeData()
-
 }
 
-function selectedCyclingShape(bikeShape:any){
-  bikeStore.setSelectedCyclingShape(bikeShape);
+// 過濾地區
+ //FIXME 初次定位不見了
+async function handleTown(e: any) {
+  let filterData = []
+  let selectedTown = e.target.value
+  if (selectedTown === '全部') {
+    bikeStore.setBikeCyclingShapeFiltered(bikeStore.bikeCyclingShape)
+    return
+  }
+  if (selectedTown != '全部') {
+    filterData = bikeStore.getCyclingShape.filter((item: TypeCyclingShape) => {
+      if (item.Town) return item.Town == selectedTown
+    })
+    bikeStore.setBikeCyclingShapeFiltered(filterData)
+  }
 }
 
+function selectedCyclingShape(bikeShape: any) {
+  bikeStore.setSelectedCyclingShape(bikeShape)
+}
 </script>
 
 <template>
   <main class="cycling-shape">
     <section class="menu">
-      <p>選擇縣市
+      <p>
+        選擇縣市
         <select v-model="selectCity" @change="handleSelect">
           <option v-for="(city, index) in cityList" :key="index" :value="city">
             {{ cityListZhTW[index] }}
           </option>
         </select>
       </p>
-      <p>請選擇區域或鄉鎮
-        <select>
-          <option>
-            我還沒做  
+      <p>
+        請選擇區域或鄉鎮
+        <select v-model="selectTown" @change="handleTown">
+          <option v-for="(town, index) in townList" :key="index" :value="town">
+            {{ town }}
           </option>
         </select>
       </p>
-      <template v-for="(bikeShape, index) in bikeStore.getCyclingShape" :key="index" :value="bikeShape">
+
+      <template v-for="(bikeShape, index) in bikeStore.bikeCyclingShapeFiltered" :key="index" :value="bikeShape">
         <div class="road" @click="selectedCyclingShape(bikeShape)">
           <div class="block">
-            <p class="roadNum">{{ index>10?index+1:'0'+(index+1) }}</p>
+            <p class="roadNum">{{ index > 10 ? index + 1 : '0' + (index + 1) }}</p>
           </div>
           <div class="block">
-            <p class="roadName">{{bikeShape.RouteName}}</p>
+            <p class="roadName">{{ bikeShape.RouteName }}</p>
             <p class="roadIfo">
-              {{bikeShape.RoadSectionStart||"無資料"}}
-              <span v-if="bikeShape.Direction=='雙向'">←</span>
+              {{ bikeShape.RoadSectionStart || '無資料' }}
+              <span v-if="bikeShape.Direction == '雙向'">←</span>
               <span>→</span>
-              {{bikeShape.RoadSectionEnd||"無資料"}}
+              {{ bikeShape.RoadSectionEnd || '無資料' }}
             </p>
-            <p class="roadIfo">車道長度： <span class="roadLength">{{bikeShape.CyclingLength/100 + "KM"}}</span></p>
+            <p class="roadIfo">
+              車道長度： <span class="roadLength">{{ bikeShape.CyclingLength / 100 + 'KM' }}</span>
+            </p>
           </div>
         </div>
       </template>
     </section>
-    
+
     <section class="cycling-shape__map">
-      <Map 
-      :cyclingShape="bikeStore.getCyclingShape" 
-      :selectedCyclingShape="bikeStore.getSelectedCyclingShape"/>
+      <Map :cyclingShape="bikeStore.bikeCyclingShapeFiltered" :selectedCyclingShape="bikeStore.getSelectedCyclingShape" />
     </section>
   </main>
 </template>
@@ -87,7 +113,7 @@ function selectedCyclingShape(bikeShape:any){
 .cycling-shape__map {
   background: #dedede;
   height: 100%;
-  width: 100%
+  width: 100%;
 }
 .menu {
   color: #fff;
@@ -101,7 +127,7 @@ function selectedCyclingShape(bikeShape:any){
   align-items: center;
 }
 
-.road{
+.road {
   width: 85%;
   border-bottom: 1px solid #1393c8;
   display: flex;
@@ -111,25 +137,25 @@ function selectedCyclingShape(bikeShape:any){
   cursor: pointer;
 }
 
-.road .block{
+.road .block {
   padding: 10px 15px 10px 25px;
 }
-.road .block:first-child{
+.road .block:first-child {
   border-right: 1px solid white;
 }
-.road .block p{
+.road .block p {
   margin: 10px 0;
 }
-.road .block p.roadNum{
+.road .block p.roadNum {
   margin: 0 0;
 }
 
-.roadName{
- font-size: 20px;
- font-weight: bold;
+.roadName {
+  font-size: 20px;
+  font-weight: bold;
 }
 
-.roadNum{
+.roadNum {
   width: 16px;
   word-wrap: break-word;
   word-break: break-all;
@@ -137,15 +163,14 @@ function selectedCyclingShape(bikeShape:any){
   line-height: 30px;
   font-weight: bold;
 }
-.roadIfo{
-   font-size: 12px;
+.roadIfo {
+  font-size: 12px;
 }
 
-.road:last-child{
+.road:last-child {
   border: none;
 }
-.roadLength{
+.roadLength {
   font-size: 14px;
 }
-
 </style>
--- src/stores/bike.ts
+++ src/stores/bike.ts
@@ -8,6 +8,7 @@ type typeBikeStore = {
   bikeStations: TypeBikeStation[],
   bikeAvailability: TypeBikeAvailability[],
   bikeCyclingShape: TypeCyclingShape[],
+  bikeCyclingShapeFiltered: TypeCyclingShape[],
   bikeNearByStations: TypeBikeStation[],
   bikeNearByAvailability: TypeBikeAvailability[],
   selectedBikeStation: any,
@@ -25,6 +26,7 @@ export const useBikeStore = defineStore({
     bikeStations: [],
     bikeAvailability: [],
     bikeCyclingShape: [],
+    bikeCyclingShapeFiltered: [],
     bikeNearByStations: [],
     bikeNearByAvailability: [],
     selectedBikeStation: {},
@@ -50,24 +52,25 @@ export const useBikeStore = defineStore({
       const resData = await api.getCyclingShapeByCityName(city)
 
       // format Geometry
-      for(let roadIndex = 0; roadIndex < resData.length ;roadIndex++){
-        let road = resData[roadIndex];
-        let roadGeometry = road.Geometry;
-        roadGeometry = roadGeometry.slice(16, roadGeometry.length).replaceAll("((","").replaceAll("))","").split("),("); // 字串格式處理
-        for(let subRoadIndex = 0;subRoadIndex<roadGeometry.length;subRoadIndex++){
-          roadGeometry[subRoadIndex] = roadGeometry[subRoadIndex].replaceAll(",",";").replaceAll(" ",",").split(";"); // 字串格式處理 + 轉陣列
-          for(let pointIndex =0;pointIndex < roadGeometry[subRoadIndex].length;pointIndex++){
-            roadGeometry[subRoadIndex][pointIndex] = roadGeometry[subRoadIndex][pointIndex].split(","); // 字串轉陣列
-             let location = []; // 經緯度順序交換
-             location.push(parseFloat(roadGeometry[subRoadIndex][pointIndex][1]))
-             location.push(parseFloat(roadGeometry[subRoadIndex][pointIndex][0]))
-             roadGeometry[subRoadIndex][pointIndex] = location;
+      for (let roadIndex = 0; roadIndex < resData.length; roadIndex++) {
+        let road = resData[roadIndex]
+        let roadGeometry = road.Geometry
+        roadGeometry = roadGeometry.slice(16, roadGeometry.length).replaceAll("((", "").replaceAll("))", "").split("),(") // 字串格式處理
+        for (let subRoadIndex = 0; subRoadIndex < roadGeometry.length; subRoadIndex++) {
+          roadGeometry[subRoadIndex] = roadGeometry[subRoadIndex].replaceAll(",", ";").replaceAll(" ", ",").split(";") // 字串格式處理 + 轉陣列
+          for (let pointIndex = 0; pointIndex < roadGeometry[subRoadIndex].length; pointIndex++) {
+            roadGeometry[subRoadIndex][pointIndex] = roadGeometry[subRoadIndex][pointIndex].split(",") // 字串轉陣列
+            let location = [] // 經緯度順序交換
+            location.push(parseFloat(roadGeometry[subRoadIndex][pointIndex][1]))
+            location.push(parseFloat(roadGeometry[subRoadIndex][pointIndex][0]))
+            roadGeometry[subRoadIndex][pointIndex] = location
           }
         }
-        resData[roadIndex].Geometry = roadGeometry;
+        resData[roadIndex].Geometry = roadGeometry
       }
 
       this.bikeCyclingShape = resData
+      this.bikeCyclingShapeFiltered = resData
       this.setLoading(false)
     },
     async getNearByStationData(lat: number, lon: number) {
@@ -94,10 +97,12 @@ export const useBikeStore = defineStore({
     setSelectedBikeStation(stationInfo: any) {
       this.selectedBikeStation = stationInfo
     },
-    setSelectedCyclingShape(cyclingShapefo:any) {
-      this.selectedCyclingShape = cyclingShapefo;
+    setSelectedCyclingShape(cyclingShapefo: any) {
+      this.selectedCyclingShape = cyclingShapefo
+    },
+    setBikeCyclingShapeFiltered(filteredData: TypeCyclingShape[]) {
+      this.bikeCyclingShapeFiltered = filteredData
     }
-
   },
 
   getters: {
@@ -116,6 +121,18 @@ export const useBikeStore = defineStore({
     getSelectedBikeStation: (state) => state.selectedBikeStation,
     getIsRent: (state) => state.isRent,
     getHasLocation: (state) => Boolean(state.userLocation[0]),
-    getSelectedCyclingShape: (state) => state.selectedCyclingShape
-  },
+    getSelectedCyclingShape: (state) => state.selectedCyclingShape,
+    getCyclingShapeTown: (state) => {
+      let TownListUnclean = state.bikeCyclingShape
+        .sort()
+        .map((item: TypeCyclingShape) => {
+          return item.Town
+        }).filter((x) => typeof x == 'string')
+
+      return ['全部', ...new Set(TownListUnclean)]
+    },
+    getBikeCyclingShapeFiltered: (state) => {
+      state.bikeCyclingShapeFiltered
+    }
+  }
 })
